63ab6d56b7d9d198e21742d90c949803da057a01
Wesley Leocadio <silvawesley374@gmail.com>
2023-01-26 16:41:05 -0300

    Criando projeto

---
+++ src/test/java/com/wesleyleocadioa/api/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleyleocadioa.api.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
